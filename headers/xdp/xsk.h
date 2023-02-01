iqraaswad/xdp-tools

commit 1aa71fd0c847748131773a10b7f1c4c6628a816c

xdp/xsk.h: Fix compilation with -std=gnu89
Commit 3c53698fff28 ("libxdp: export inline helpers as symbols in
libxdp") used C99 inline semantics to rely on inline without extern not
emitting a symbol, and extern inline declaration doing that instead. On
C89, these C99 inline semantics are not followed.

Instead of requiring users to migrate away from C89, we can still retain
compatibility by marking XDP_ALWAYS_INLINE as static inline, while
ensuring libxdp continues to expose these symbols.

Closes: #173
Fixes: 3c53698fff28 ("libxdp: export inline helpers as symbols in libxdp")
Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>

headers/xdp/xsk.h
@@ -21,7 +21,11 @@
 extern "C" {
 #endif
 
+#ifdef __GNUC_STDC_INLINE__
 #define XDP_ALWAYS_INLINE inline __attribute__((__always_inline__))
+#elif __GNUC_GNU_INLINE__
+#define XDP_ALWAYS_INLINE static inline __attribute__((__always_inline__))
+#endif
 
 /* Do not access these members directly. Use the functions below. */
 #define DEFINE_XSK_RING(name) \

commit 7ec99a19017f98f28dfe856ce18e3033595319b9

xdp/xsk.h: Fix undefined XDP_ALWAYS_INLINE with sparse
Sparse checker doesn't have __GNUC_GNU_INLINE__ or __GNUC_STDC_INLINE__
defined, the same will be true for any non-GNU compiler.

This is causing build failures due to XDP_ALWAYS_INLINE being undefined.

This is potentially not the best way of fixing the problem, but
it solves the current issue.

Closes: #279
Fixes: 1aa71fd0c847 ("xdp/xsk.h: Fix compilation with -std=gnu89")
Reported-by: David Marchand <dmarchan@redhat.com>
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

headers/xdp/xsk.h
@@ -25,6 +25,8 @@ extern "C" {
 #define XDP_ALWAYS_INLINE inline __attribute__((__always_inline__))
 #elif __GNUC_GNU_INLINE__
 #define XDP_ALWAYS_INLINE static inline __attribute__((__always_inline__))
+#else
+#define XDP_ALWAYS_INLINE static inline
 #endif
 
 /* Do not access these members directly. Use the functions below. */